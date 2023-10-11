04877b6458403eb040568a53cd83f0e85a739e3d
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-10 20:22:39 -0400

    make css changes to display cards

--- src/components/StarshipDetails.jsx
+++ src/components/StarshipDetails.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
-
 const StarshipDetails = ({starship}) => {
   console.log(starship)
   console.log(starship.url)
 
   return (
-    <>
-    <div>
+    <main className="card">
+    <div className="card-container">
       <Link to={`/ships/${starship.url.match(/\d+/)}`}>{starship.name} </Link>
     </div>
-    </>
+
+
+
+
+    </main>
   );
 }
 export default StarshipDetails;
